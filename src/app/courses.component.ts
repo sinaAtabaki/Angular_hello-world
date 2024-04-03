57bc38ecb4fcfaa2536f2ee6b2239e9bb5acff53
Sina Atabaki <sina.atabaki@solutions-factory.de>
2024-04-03 15:52:11 +0200

    Lesson 32 Add Course Component

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,9 +2,13 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>',
+    template: '<h2>{{ "Title: " + getTitle() }}</h2>',
     standalone: true
 })
 export class CoursesComponent{
+    title = "List of courses";
 
+    getTitle() {
+        return this.title;
+    }
 }
