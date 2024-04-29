ede16da01c998a6bb6adc0a712a7f615c47be887
sinaAtabaki <104894597+sinaAtabaki@users.noreply.github.com>
2024-04-29 15:24:19 +0200

    add contact-form Component

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -13,6 +13,10 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+  contactMethods = [
+    { id: 1, name: 'Email' },
+    { id: 2, name: 'Phone' }
+  ]
   log(x: any) {console.log(x)};
 
   submit(f: any){
